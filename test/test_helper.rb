4f586b5f1c9e4e21089ed83498b098a0348d7ede
Jean Boussier <jean.boussier@gmail.com>
2021-01-25 10:05:21 +0100

    Run GC.verify_compaction_references on CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,10 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "heap_profiler/full"
 
+if GC.respond_to?(:verify_compaction_references)
+  GC.verify_compaction_references(double_heap: true, toward: :empty)
+end
+
 require "tempfile"
 require "tmpdir"
 require "stringio"
